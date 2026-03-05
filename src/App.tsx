fbc9a777c1fff706e9abe4444108899fb79d21b1
Sheraz Dev <sherazk.dev@gmail.com>
2026-03-05 22:06:19 +0500

    feat: Refactor App component to use lazy loading for routes and add SingleCategory route

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import LoginPage from "./pages/auth/LoginPage";
-import Sell from "./pages/Sellnow/Sell";
-import SingleProduct from "./pages/SingleProductPage/SingleProduct";
+import { lazy, Suspense } from "react";
 
-
-
+const Home = lazy(() => import("./pages/Home/Home"));
+const LoginPage = lazy(() => import("./pages/auth/Auth"));
+const Sell = lazy(() => import("./pages/Sellnow/Sell"));
+const SingleCategory = lazy(() => import("./pages/SingleCategory/SingleCategory"));
+const ProductPage = lazy(() => import("./pages/SingleProduct/ProductPage"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/sell" element={<Sell/>}/>
-      {/* <Route path="/category/:categoryName" element={<SingleProduct/>}/> */} //route by name 
+    <Suspense fallback={<div>Loading...</div>}>
 
-      <Route path="/category/:categoryId" element={<SingleProduct />} />  //route by id 
+      <Routes>
 
-      
+        <Route path="/" element={<Home />} />
 
-      
+        <Route path="/login" element={<LoginPage />} />
 
-    </Routes>
+        <Route path="/sell" element={<Sell />} />
+
+        <Route path="/category/:categoryId" element={<SingleCategory />} />
+
+        <Route path="/product/:productId" element={<ProductPage />} />
+
+      </Routes>
+
+    </Suspense>
   );
 }
 
